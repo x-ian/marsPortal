53061c9033aa66ccfb6d43e48aa8dc09e87b2895
Christian Neumann <x-ian@x-ian.net>
2015-02-25 11:38:20 +0200

    newer daily_accounting approach

--- daloradius-accounting/accounting-snapshot-end-of-day.sh
+++ daloradius-accounting/accounting-snapshot-end-of-day.sh
@@ -4,11 +4,10 @@ BASEDIR=`dirname $0`
 source $BASEDIR/../config.txt
 
 /usr/bin/mysql -u `echo $DR_MYSQL_USER` -p`echo $DR_MYSQL_PASSWD` radius <<EOF
-set @today = date_format(now(), '%Y-%m-%d');
-
--- end of day
 INSERT INTO daily_accounting (username, day, day_end, inputoctets_day_end, outputoctets_day_end)
-  SELECT DISTINCT(radacct.username), @today, now(), SUM(radacct.acctinputoctets), SUM(radacct.acctoutputoctets)
-  FROM radacct WHERE acctstarttime > @today GROUP BY username
+SELECT DISTINCT(radacct.username), date_format(now(), '%Y-%m-%d'), now(), SUM(radacct.acctinputoctets), SUM(radacct.acctoutputoctets)
+FROM radacct 
+WHERE UNIX_TIMESTAMP(AcctStartTime) + AcctSessionTime > UNIX_TIMESTAMP(date_format(now(), '%Y-%m-%d')) 
+GROUP BY username
 ON DUPLICATE KEY UPDATE username=VALUES(username), day=VALUES(day), day_end=now(), inputoctets_day_end=VALUES(inputoctets_day_end), outputoctets_day_end=VALUES(outputoctets_day_end);
 EOF
