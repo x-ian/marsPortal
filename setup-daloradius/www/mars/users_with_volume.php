951e5b65ad93b2944a3f1078238004e36c38b2d6
Christian Neumann <x-ian@x-ian.net>
2015-03-02 19:20:52 +0200

    refactored reports

--- setup-daloradius/www/mars/users_with_volume.php
+++ setup-daloradius/www/mars/users_with_volume.php
@@ -28,10 +28,43 @@
     $daysago7 = date('Y-m-d', strtotime('-6 days'));
     $daysago30 = date('Y-m-d', strtotime('-29 days'));
   
-echo "<table><tr><th>Username</th><th>Group</th><th>Name</th><th>Email</th><th>Organization</th><th>Computername</th><th>Vendor</th><th>Down last 7 days</th><th>Up last 7 days</th></tr>";
+echo "
+<table>
+	<tr>
+		<th>Username</th>
+		<th>Group</th>
+		<th>Name</th>
+		<th>Email</th>
+		<th>Organization</th>
+		<th>Computername</th>
+		<th>Vendor</th>
+		<th>Down last 7 days</th>
+		<th>Up last 7 days</th>
+	</tr>";
 
   function users($startday, $endday) {
-return 'select * from ( (SELECT distinct(radacct.UserName) as username, radusergroup.groupname as groupname, userinfo.lastname as name, userinfo.email as email, userinfo.company as company, userinfo.address as address, userinfo.city as city, ROUND((sum(radacct.AcctOutputOctets)/1000000)) as download, ROUND((sum(radacct.AcctInputOctets)/1000000)) as upload FROM radacct     LEFT JOIN radusergroup ON radacct.username=radusergroup.username LEFT OUTER JOIN userinfo ON radacct.username=userinfo.username    WHERE (AcctStopTime > "0000-00-00 00:00:01" AND AcctStartTime> "' . $startday . '" AND AcctStartTime<date(date_add("' .$endday . '", INTERVAL +1 DAY))) OR ((radacct.AcctStopTime IS NULL OR radacct.AcctStopTime = "0000-00-00 00:00:00") AND AcctStartTime<date(date_add("' . $endday . '", INTERVAL +1 DAY))) group by UserName) union (select ui.username, "", "", "", "", "", "", "0" as download, "0" as upload from userinfo ui) ) as t1 group by username;';  
+return '
+select * from ( 
+	(SELECT distinct(radacct.UserName) as username, 
+		radusergroup.groupname as groupname, 
+		userinfo.lastname as name, 
+		userinfo.email as email, 
+		userinfo.company as company, 
+		userinfo.address as address, 
+		userinfo.city as city, 
+		ROUND((sum(radacct.AcctOutputOctets)/1000000)) as download, 
+		ROUND((sum(radacct.AcctInputOctets)/1000000)) as upload 
+	FROM radacct 
+	    LEFT JOIN radusergroup ON radacct.username=radusergroup.username 
+		LEFT OUTER JOIN userinfo ON radacct.username=userinfo.username
+		    WHERE (AcctStopTime > "0000-00-00 00:00:01" AND AcctStartTime> "' . $startday . '" AND AcctStartTime<date(date_add("' .$endday . '", INTERVAL +1 DAY)))
+			OR ((radacct.AcctStopTime IS NULL OR radacct.AcctStopTime = "0000-00-00 00:00:00") AND AcctStartTime<date(date_add("' . $endday . '", INTERVAL +1 DAY))) 
+			group by UserName
+	) 
+	union 
+	(select ui.username, "", "", "", "", "", "", "0" as download, "0" as upload from userinfo ui) 
+) as t1 group by username;
+';  
   }
   
 $all_users = query(users($daysago7, $today));
@@ -49,6 +82,4 @@ while ($row = mysql_fetch_assoc($all_users)) {
 	echo "</tr>";
 }
 mysql_free_result($all_users);
-
 ?>
-
