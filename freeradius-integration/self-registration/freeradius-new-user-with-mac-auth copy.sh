0d48a8521637fdee03ea64bd804e41ca8e22e557
Christian Neumann <x-ian@x-ian.net>
2015-04-21 10:24:12 +0200

    fr self registration

---
+++ freeradius-integration/self-registration/freeradius-new-user-with-mac-auth copy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Add new user with MAC Auth to DR
+
+BASEDIR=`dirname $0`
+source $BASEDIR/../../config.txt
+PATH=$PATH:/usr/local/bin
+
+MAC=$1
+FIRSTNAME=$2
+LASTNAME=$3
+EMAIL=$4
+COMPANY=$5
+GROUP="$6"
+IP=$7
+HOSTNAME=$8
+MAC_VENDOR=$9
+PRIMARY_DEVICE=${10}
+
+#INSERT INTO radcheck (id,Username,Attribute,op,Value) VALUES (0, '111', 'Auth-Type', ':=', 'Accept') 
+#INSERT INTO radusergroup (UserName,GroupName,priority) VALUES ('111', 'Users',0) 
+#INSERT INTO userinfo (id, username, firstname, lastname, email, department, company, workphone, homephone, mobilephone, address, city, state, country, zip, notes, changeuserinfo, portalloginpassword, enableportallogin, creationdate, creationby, updatedate, updateby) VALUES (0, '111', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '0', '', '0', '2015-04-21 02:27:14', 'administrator', NULL, NULL) 
+
+mysql -u $MYSQL_USER -p$MYSQL_PASSWD radius <<EOF
+	INSERT INTO radcheck (Username,Attribute,op,Value) VALUES ("$MAC", "Auth-Type", ":=", "Accept");
+	INSERT INTO radusergroup (UserName,GroupName,priority) VALUES ("$MAC", "$GROUP",0);
+EOF
