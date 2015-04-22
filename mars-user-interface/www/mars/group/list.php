c105ab86c550251d81929b8e614764cd27eecb2f
Christian Neumann <x-ian@x-ian.net>
2015-04-22 18:03:54 +0200

    groups

--- mars-user-interface/www/mars/group/list.php
+++ mars-user-interface/www/mars/group/list.php
@@ -21,21 +21,34 @@ echo "<td><b>Concurrent User</b></td>";
 echo "<td><b>Auth Type</b></td>"; 
 echo "<td><b>Reply Message</b></td>"; 
 echo "</tr>"; 
-$result = mysql_query("SELECT * FROM `group`") or trigger_error(mysql_error()); 
+$result = mysql_query('
+select rr1.groupname, 
+	(select value from radgroupcheck r2 where attribute="mars-Max-Concurrent-Devices" and r2.groupname = r1.groupname)  "Max Concurrent Users", 
+	(select value from radgroupcheck r5 where attribute="mars-Output-Megabytes-Daily-Total" and r5.groupname = r1.groupname)  "Max Daily Down", 
+	(select value from radgroupcheck r6 where attribute="mars-Input-Megabytes-Daily-Total" and r6.groupname = r1.groupname)  "Max Daily Up", 
+	(select value from radgroupcheck r9 where attribute="mars-Output-Megabytes-Daily-Work-Hours" and r9.groupname = r1.groupname)  "Max Work Hours Down", 
+	(select value from radgroupcheck r10 where attribute="mars-Input-Megabytes-Daily-Work-Hours" and r10.groupname = r1.groupname)  "Max Work Hours Up", 
+	(select value from radgroupreply rr2 where attribute ="Session-Timeout" and rr2.groupname = rr1.groupname) "Session Timeout", 
+	(select value from radgroupreply rr3 where attribute ="WISPr-Bandwidth-Max-Up" and rr3.groupname = rr1.groupname) "WISPr-Bandwidth-Max-Up", 
+	(select value from radgroupreply rr4 where attribute ="WISPr-Bandwidth-Max-Down" and rr4.groupname = rr1.groupname) "WISPr-Bandwidth-Max-Down",
+"todo", "todo"
+from radgroupreply rr1 left join radgroupcheck r1 on rr1.groupname = r1.groupname 
+group by rr1.groupname;') or trigger_error(mysql_error()); 
 while($row = mysql_fetch_array($result)){ 
 foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
 echo "<tr>";  
-echo "<td>" . nl2br( $row['groupname']) . "</td>";  
-echo "<td>" . nl2br( $row['work_total_input']) . "</td>";  
-echo "<td>" . nl2br( $row['work_total_output']) . "</td>";  
-echo "<td>" . nl2br( $row['day_total_input']) . "</td>";  
-echo "<td>" . nl2br( $row['day_total_output']) . "</td>";  
-echo "<td>" . nl2br( $row['bandwidth_up']) . "</td>";  
-echo "<td>" . nl2br( $row['bandwidth_down']) . "</td>";  
-echo "<td>" . nl2br( $row['session_timeout']) . "</td>";  
-echo "<td>" . nl2br( $row['concurrent_user']) . "</td>";  
-echo "<td>" . nl2br( $row['auth_type']) . "</td>";  
-echo "<td>" . nl2br( $row['reply_message']) . "</td>";  
+
+echo "<td>" . nl2br( $row['groupname']) . "</td>";
+echo "<td>" . nl2br( $row['Max Work Hours Up']) . "</td>";
+echo "<td>" . nl2br( $row['Max Work Hours Down']) . "</td>";
+echo "<td>" . nl2br( $row['WISPr-Bandwidth-Max-Up']) . "</td>";
+echo "<td>" . nl2br( $row['WISPr-Bandwidth-Max-Down']) . "</td>";
+echo "<td>" . nl2br( $row['Session Timeout']) . "</td>";
+echo "<td>" . nl2br( $row['Max Concurrent Users']) . "</td>";
+echo "<td>" . nl2br( $row['Max Daily Up']) . "</td>";
+echo "<td>" . nl2br( $row['Max Daily Down']) . "</td>";
+echo "<td>" . nl2br( $row['Auth Type']) . "</td>";  
+echo "<td>" . nl2br( $row['Reply Message']) . "</td>";  
 echo "<td><a href=edit.php?id={$row['id']}>Edit</a></td><td><a href=duplicate.php?id={$row['id']}>Duplicate</a></td><td><a href=delete.php?id={$row['id']}>Delete</a></td> "; 
 echo "</tr>"; 
 } 
