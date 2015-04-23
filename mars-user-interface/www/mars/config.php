d7dc1d0a496d3ffdc82cb56d11309b6f16b54173
Charlie Root <root@pfSense.localdomain>
2015-04-23 11:51:50 +0200

    config.txt used for PHP

--- mars-user-interface/www/mars/config.php
+++ mars-user-interface/www/mars/config.php
@@ -2,14 +2,16 @@
 // parse marsPortal config file
 $fh=fopen("/home/marsPortal/config.txt", "r");
 while ($line=fgets($fh, 80)) {
-  if (preg_match('/^[a-z]+=("[a-z0-9]+"|[0-9]+)$/', $line)) {
+  if (!preg_match('/^#/', $line) && preg_match('/=/', $line)) {
     $line_a=explode("=", $line);
-    $line_a[0]=$line_a[1];
+	$param = preg_replace( "/\r|\n/", "", $line_a[0] );
+	$value = preg_replace( "/\r|\n/", "", $line_a[1] );
+    ${$param}=$value;
   }
 }
 
 // connect to db
-$link = mysql_connect('localhost', $MYSQL_USER, $MYSQL_PASS);
+$link = mysql_connect('localhost', $MYSQL_USER, $MYSQL_PASSWD);
 if (!$link) {
     die('Not connected : ' . mysql_error());
 }
