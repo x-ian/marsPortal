7582a857742a4794e0ec095c0d6fb13a196b099c
Christian Neumann <x-ian@x-ian.net>
2015-04-23 19:52:25 +0200

    restructuring daily accounting

--- daloradius-accounting/accounting-snapshot-v2.sh
+++ daloradius-accounting/accounting-snapshot-v2.sh
@@ -1,42 +1,55 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 BASEDIR=`dirname $0`
 source /home/marsPortal/config.txt
+# all times in hoursminutes, hours without leading zero, minutes with leading zero
+DAY_START=5
+WORK_START=730
+LUNCH_START=1200
+LUNCH_END=1330
+WORK_END=1700
+DAY_END=2355
+# day in week, mo=1, su=7
+WORK_DAYS=1,2,3,4,5
 
-if [ "$1" == "day_offset" ]; then
-	TIME_COL=day_offset
-	INPUT_COL=day_offset_input
-	OUTPUT_COL=day_offset_output
-elif [ "$1" == "work_offset" ]; then
-	TIME_COL=work_offset
-	INPUT_COL=work_offset_input
-	OUTPUT_COL=work_offset_output
-elif [ "$1" == "lunch_offset" ]; then
-	TIME_COL=lunch_offset
-	INPUT_COL=lunch_offset_input
-	OUTPUT_COL=lunch_offset_output
-elif [ "$1" == "lunch_total" ]; then
-	TIME_COL=lunch_total
-	INPUT_COL=lunch_total_input
-	OUTPUT_COL=lunch_total_output
-elif [ "$1" == "work_total" ]; then
-	TIME_COL=work_total
-	INPUT_COL=work_total_input
-	OUTPUT_COL=work_total_output
-elif [ "$1" == "day_total" ]; then
-	TIME_COL=day_total
-	INPUT_COL=day_total_input
-	OUTPUT_COL=day_total_output
-else
-	echo "Unknown value for parameter #1 ($1). Exiting."
-	exit 1
+# current time and day in week
+NOW=`date +%-H%M`
+NOW_DAY=`date +%u`
+
+DAY_OFFSET_ALREADY_SET=$(/usr/local/bin/mysql -u $MYSQL_USER -p$MYSQL_PASSWD radius -se 'select id from daily_accounting_v2 where day_offset is not null and day=current_date();' | wc -l)
+
+if [ "$DAY_START" -le "$NOW" ] && [ "$DAY_OFFSET_ALREADY_SET" -eq "0" ]; then
+	update_accounting "day_offset", "day_offset_input", "day_offset_output"
+fi
+if [[ $WORK_DAYS == *"$NOW_DAY"* ]] && [ "$WORK_START" -le "$NOW" ] && work_offset not set
+	update_accounting "work_offset", "work_offset_input", "work_offset_output"
+fi
+if [[ $WORK_DAYS == *"$NOW_DAY"* ]] && [ "$LUNCH_START" -le "$NOW" ] && lunch_offset not set
+	update_accounting "lunch_offset", "lunch_offset_input", "lunch_offset_output"
 fi
 
-/usr/bin/mysql -u `echo $DR_MYSQL_USER` -p`echo $DR_MYSQL_PASSWD` radius <<EOF
+if day_offset set
+	update_accounting "day_total", "day_total_input", "day_total_output"
+fi
+if is workday && work_offset set && work_end > now (&& not lunchtime?)
+	update_accounting "work_total", "work_total_input", "work_total_output"
+fi
+if is workday && lunch_offset set && lunch_end > now
+	update_accounting "lunch_total", "lunch_total_input", "lunch_total_output"
+fi
+
+update_accounting() {
+	TIME_COL=$1
+	INPUT_COL=$2
+	OUTPUT_COL=$3
+	
+	/usr/local/bin/mysql -u `echo $MYSQL_USER` -p`echo $MYSQL_PASSWD` radius <<EOF
 INSERT INTO daily_accounting_v2 (username, day, $TIME_COL, $INPUT_COL, $OUTPUT_COL)
 SELECT DISTINCT(radacct.username), date_format(now(), '%Y-%m-%d'), now(), SUM(radacct.acctinputoctets), SUM(radacct.acctoutputoctets)
   FROM radacct 
   WHERE UNIX_TIMESTAMP(AcctStartTime) + AcctSessionTime > UNIX_TIMESTAMP(date_format(now(), '%Y-%m-%d')) 
   GROUP BY username
+
   ON DUPLICATE KEY UPDATE username=VALUES(username), day=VALUES(day), $TIME_COL=now(), $INPUT_COL=VALUES($INPUT_COL), $OUTPUT_COL=VALUES($OUTPUT_COL);
 EOF
+}
