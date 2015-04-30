3de600881300d07772b85a7d68d825a2b86113bd
Charlie Root <root@pfSense.localdomain>
2015-04-30 22:27:59 +0200

    first working radius througput report

--- mars-user-interface/www/mars/reports/recent_top_X.php
+++ mars-user-interface/www/mars/reports/recent_top_X.php
@@ -7,38 +7,51 @@ include '../menu.php';
 
 <? 
 include('../config.php'); 
+$order = $_GET['order']; 
 
-function top_upordown($startday, $endday, $topX, $upordown) {
-/*
-select da.username, snap.datetime, da.day_total_input - snap.input, da.day_total_output - snap.output
-from accounting_snapshot snap, daily_accounting_v2 da
-where da.username = snap.username and da.day = '2015-04-30' and snap.datetime = '2015-04-30 18:14:35';
-*/
-return "
-	SELECT 
-		distinct(radacct.UserName) as username, 
-		radusergroup.groupname as groupname, 
-		CONCAT_WS(' ', userinfo.firstname, userinfo.lastname) as name, 
-		userinfo.department as department, 
-		userinfo.email as email, 
-		userinfo.company as company, 
-		userinfo.address as address, 
-		userinfo.city as city,
-		ROUND((sum(radacct.AcctOutputOctets)/1000000)) as Download,
-		ROUND((sum(radacct.AcctInputOctets)/1000000)) as Upload
-	FROM radacct     
-		LEFT JOIN radusergroup ON radacct.username=radusergroup.username 
-		LEFT JOIN userinfo ON radacct.username=userinfo.username    
-	WHERE (AcctStopTime > \"0000-00-00 00:00:01\" AND AcctStartTime>\"" . $startday . "\" AND AcctStartTime<date(date_add(\"" . $endday . "\", INTERVAL +1 DAY))) 
-		OR 
-		((radacct.AcctStopTime IS NULL OR radacct.AcctStopTime = \"0000-00-00 00:00:00\") AND AcctStartTime<date(date_add(\"" . $endday . "\", INTERVAL +1 DAY))) 
-		group by UserName order by " . $upordown . " desc limit " . $topX . ";";  
+?>
+
+<a href="recent_top_X.php?order=input">Sort by Input</a> <a href="recent_top_X.php?order=input">Sort by Output</a>
+
+<br/>
+
+<table>
+	<tr>
+		<th>Username</th>
+		<th>Throughput In</th>
+		<th>Throughput Out</th>
+	</tr>
+
+<?
+function throughput_upordown($topX, $order) {
+	return "select da.username, (da.day_total_input - snap.input) / 1000000 as input, (da.day_total_output - snap.output) / 1000000 as output
+	from accounting_snapshot snap, daily_accounting_v2 da
+	where da.username = snap.username and da.day = date_format(now(), '%Y-%m-%d') and date_format(snap.datetime, '%Y-%m-%d') = date_format(now(), '%Y-%m-%d')
+	ORDER BY " . $order . " LIMIT " . $topX;
 }
 
+$result = mysql_query(throughput_upordown(10, "input"))  or trigger_error(mysql_error()); 
 
+while($row = mysql_fetch_array($result)){ 
+	foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
+	echo "<tr>";  
+	echo "<td>" . nl2br( $row[0]) . "</td>";
+	echo "<td>" . nl2br( $row[1]) . "</td>";
+	echo "<td>" . nl2br( $row[2]) . "</td>";
+	echo "</tr>";  
+}
+?>
+</table>
+
+<br/>
+
+<?
+$result = mysql_query("select datetime	from accounting_snapshot limit 1") or trigger_error(mysql_error()); 
+if($row = mysql_fetch_array($result)){ 
+	foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
+	echo "Transfer volumes since " . $row[0];  
+}
+?>
 
 </div>
 </body>
-
-
-
