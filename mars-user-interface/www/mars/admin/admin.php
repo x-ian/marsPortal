6fd8243c3ed98561a5c04d2446f41a9e880dfb48
Christian Neumann <x-ian@x-ian.net>
2015-05-07 08:32:34 +0200

    some admin enhacements

---
+++ mars-user-interface/www/mars/admin/admin.php
@@ -0,0 +1,20 @@
+<? 
+include 'menu.php'; 
+?>
+
+<!-- begin page-specific content ########################################### -->
+    <div id="main">
+
+Admin: 
+
+<p><a href="">Temporarily block/slow down groups</a></p><hr/>
+<p><a href="">Notify when device comes online</a></p><hr/>
+<p><a href="">Cleanup devices not seen for last x months</a></p><hr/>
+
+Config:
+
+<p><a href="">Reset all accounting data</a></p><hr/>
+<p><a href="./network-devices-to-monitor.php">Edit network devices to monitor</a></p><hr/>
+
+</div>
+</body>
