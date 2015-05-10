aff82435efd9ead7d5694861748d4e22b7e3b0f7
Christian Neumann <x-ian@x-ian.net>
2015-05-10 14:31:28 +0200

    third

--- mars-user-interface/www/mars/reports/online-users.php
+++ mars-user-interface/www/mars/reports/online-users.php
@@ -10,7 +10,7 @@ include('../config.php');
 
 function online() {
 	return "
-		SELECT radacct.username AS username, userinfo.firstname AS firstname, userinfo.lastname AS lastname, userinfo.mac_vendor AS mac_vendor, userinfo.hostname AS hostname, userinfo.email AS email, userinfo.department AS department, userinfo.organisation AS organisation, userinfo.registration_date AS registration_date, radacct.FramedIPAddress, radacct.AcctStartTime, radacct.AcctSessionTime, radacct.AcctInputOctets AS Upload, radacct.AcctOutputOctets AS Download 
+		SELECT radacct.username AS username, userinfo.firstname AS firstname, userinfo.lastname AS lastname, userinfo.mac_vendor AS mac_vendor, userinfo.hostname AS hostname, userinfo.email AS email, userinfo.department AS department, userinfo.organisation AS organisation, userinfo.registration_date AS registration_date, radacct.FramedIPAddress as ipaddress, radacct.AcctStartTime as acctstart, radacct.AcctSessionTime, radacct.AcctInputOctets AS Upload, radacct.AcctOutputOctets AS Download 
 		FROM radacct LEFT JOIN userinfo ON (radacct.Username = userinfo.Username) 
 		WHERE (radacct.AcctStopTime IS NULL OR radacct.AcctStopTime = '0000-00-00 00:00:00') AND DATE_ADD(radacct.acctstarttime, INTERVAL radacct.acctsessiontime second) > DATE_ADD(NOW(), INTERVAL -10 MINUTE) AND (radacct.Username LIKE '%') 
 		ORDER BY radacctid asc;";
@@ -39,7 +39,16 @@ function online() {
 $result = mysql_query(online()) or trigger_error(mysql_error()); 
 while($row = mysql_fetch_array($result)){ 
 	foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
-	echo "<tr><td>" . $row['username'] . "</td><td>" . $row['lastname'] . "</td><td>" . $row['mac_vendor'] . "</td></tr>";
+	echo "<tr>";
+	echo "<td><a href=edit.php?username=" . $row['username'] . ">" . $row['username'] . "</a></td>";
+	echo "<td>" . $row['firstname'] . " " . $row['lastname'] . "</td>";
+	echo "<td>" . $row['email'] . "</td>";
+	echo "<td>" . $row['mac_vendor'] . "</td>";
+	echo "<td>" . $row['hostname'] . "</td>";
+	echo "<td>" . $row['organisation'] . "</td>";
+	echo "<td>" . $row['ipaddress'] . "</td>";
+	echo "<td>" . $row['acctstart'] . "</td>";
+	echo "</tr>";
 }
 ?>
 </table>
