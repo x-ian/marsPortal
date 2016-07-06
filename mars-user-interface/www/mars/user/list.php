1dea95cf373285c3aed1852ee0bdbc838393ba72
Christian Neuman <x-ian@x-ian.net>
2016-07-06 07:35:44 +0200

    simplified php

--- mars-user-interface/www/mars/user/list.php
+++ mars-user-interface/www/mars/user/list.php
@@ -5,6 +5,33 @@ include '../menu.php';
 <!-- begin page-specific content ########################################### -->
     <div id="main">
 
+<?
+if (isset($_POST['submitted'])) { 
+	$mac1 = $_POST['mac'][0]; 
+	$mac2 = $_POST['mac'][1]; 
+	
+	$mac_source = mysql_query("select * from userinfo where username = '" . $mac1 . "';");
+	$firstname = "";
+	$lastname = "";
+	$department = "";
+	if ($row = mysql_fetch_assoc($mac_source)) {
+		$firstname = $row['firstname'];
+		$lastname = $row['lastname'];
+		$department = $row['department'];
+	}
+	$mac_source_group = mysql_query("select * from radusergroup where username = '" . $mac1 . "'");
+	$group = "";
+	if ($row_group = mysql_fetch_assoc($mac_source_group)) {
+		$group = $row_group['groupname'];
+	}
+		
+	$update_mac_target = mysql_query(" UPDATE userinfo set firstname = '$firstname', lastname = '$lastname', department = '$department' where username = '$mac2' ");
+	$update_mac_target_group = mysql_query(" UPDATE radusergroup set groupname = '$group' where username = '$mac2' ");
+
+	echo "Devices joined.<br />"; 
+}
+?>
+
 <script>
 	function ensureTwoCheckboxes() {
 		var nodeList = document.getElementsByName('mac[]')
@@ -15,16 +42,14 @@ include '../menu.php';
 		    }
 		}
 		if (checked == 2) {
-			document.getElementById('accept').disabled=false;
+			document.getElementById('submitted').disabled=false;
 		} else {
-			document.getElementById('accept').disabled=true;			
+			document.getElementById('submitted').disabled=true;			
 		}
 	}
 </script>
+	
 <? 
-
-include '../config.php'; 
-
 echo "<table>"; 
 echo "<tr>"; 
 echo "<td><b>Name</b></td>"; 
@@ -38,7 +63,7 @@ echo "<td><b>Email</b></td>";
 echo "<td><b>Organisation</b></td>"; 
 echo "<td><b>Notes</b></td>"; 
 echo "</tr>"; 
-echo "<form action='join-devices-post.php' method='POST'>";
+echo "<form action='' method='POST'>";
 
 $result = mysql_query("SELECT u.username AS username, u.firstname AS firstname, u.lastname AS lastname, u.mac_vendor AS mac_vendor, u.hostname AS hostname, u.email AS email, u.department AS department, u.organisation AS organisation, u.registration_date AS registration_date, u.initial_ip AS initial_ip, u.notes AS notes, radusergroup.groupname AS groupname FROM userinfo AS u LEFT JOIN radusergroup on u.username = radusergroup.username ORDER BY lastname, firstname ASC") or trigger_error(mysql_error()); 
 $previous_name = "";
@@ -65,7 +90,7 @@ echo "<td>" . nl2br( $row['organisation']) . "</td>";
 echo "<td>" . nl2br( $row['notes']) . "</td>";  
 echo "</tr>"; 
 } 
-echo "<tr><td><br/></td></tr><tr><td colspan='10'><input id='accept' name='accept' type='submit' value='Join device entries' disabled/> (unifies name, department & group; only active when two devices are selected)</td></tr>";
+echo "<tr><td><br/></td></tr><tr><td colspan='10'><input id='submitted' name='submitted' type='submit' value='Join device entries' disabled/> (unifies name, department & group; only active when two devices are selected)</td></tr>";
 echo "</form></table>"; 
 ?>
 </div>
