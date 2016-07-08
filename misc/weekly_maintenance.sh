b0b6b6f64bf31fca23e396bd4bb941ec45b81015
x-ian <x-ian@x-ian.net>
2016-07-08 13:12:12 +0200

    testing

--- misc/weekly_maintenance.sh
+++ misc/weekly_maintenance.sh
@@ -9,7 +9,7 @@ BASEDIR=/home/marsPortal
 source $BASEDIR/config.txt
  
 # reset ntop traffic stats
-/usr/local/bin/wget --user `echo $NTOP_USER` --password `echo $NTOP_PASSWD` http://`echo $PF_IP`:3000/resetStats.html
+#/usr/local/bin/wget --user `echo $NTOP_USER` --password `echo $NTOP_PASSWD` http://`echo $PF_IP`:3000/resetStats.html
 
 # compacting squid cache (http://doc.pfsense.org/index.php/Squid_Package_Tuning)
 /usr/local/sbin/squid -k rotate
@@ -21,7 +21,7 @@ source $BASEDIR/config.txt
 /usr/local/sbin/squid -z
 
 # some internal backup
-#/home/marsPortal/misc/do-backup.sh
+/home/marsPortal/misc/do-backup.sh
 
 # clean up DHCP leases as they seem to be never removed. ideally this should maybe be done monthly or quarterly
 /bin/rm -f /var/dhcpd/var/db/dhcpd.leases
@@ -48,5 +48,6 @@ fi
 /home/marsPortal/misc/heartbeat.sh
 
 # just in case, restart once in a while
+sleep 60
 /sbin/reboot
 
