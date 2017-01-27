692f95268a73f9ebd5be3036099acc55d55307e9
x-ian <x-ian@x-ian.net>
2017-01-27 08:04:05 +0100

    stuff

--- misc/start-ssh-tunnel.sh
+++ misc/start-ssh-tunnel.sh
@@ -4,5 +4,9 @@ BASEDIR=/home/marsPortal
 
 source $BASEDIR/config.txt
 
+echo "Stopping all tunnels"
 /usr/bin/killall ssh
+/bin/sleep 5
+
+echo "Starting new tunnel, abort with CTRL-C"
 /usr/local/bin/autossh -M 0 -o "StrictHostKeyChecking no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -o BatchMode=yes -R $SSH_TUNNEL_PORT:localhost:22 -N ssh-tunnel@marsgeneral.com 
