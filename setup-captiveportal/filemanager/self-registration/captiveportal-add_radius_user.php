d8f871fdc2f45d787e8573d35639a4c5b828d1b5
x-ian <x-ian@x-ian.net>
2017-03-05 21:25:16 +0100

    simple self-reg derived from azpu self reg

--- setup-captiveportal/filemanager/self-registration/captiveportal-add_radius_user.php
+++ setup-captiveportal/filemanager/self-registration/captiveportal-add_radius_user.php
@@ -1,33 +1,46 @@
 <html>
 <head>
-<meta http-equiv="Refresh" content="3; url=http://www.google.mw/" />
+	<meta http-equiv="Refresh" content="3; url=http://www.marsgeneral.com/" />
 </head>
+
 <body>
 <span style="font-variant:small-caps; font-size:200%">
-	<p align="center">
-		Abwenzi Pa Za Umoyo / Partners In Health
+	<table align="center">
+		<tr><td><img src="/captiveportal-mars.jpg" /></td><td>Portal</td></tr>
+	</table>
+</span> 
+
+<hr/><br/>
+
+<div align="center">
+
+	<pEverybody gets connected - Hopefully you by now as well!</p>
+	<p><?php
+		$ip=$_SERVER['REMOTE_ADDR'];
+		$name=$_REQUEST['name'];
+		$email="";
+		$owner="";
+		$primary="";
+	    echo "The registration process should be done for " . $name . " (" . $email . ") from " . $ip;
+		/* echo "/home/marsPortal/freeradius-integration/self-registration/captive-portal-add_user_to_radius.sh " 
+		. $ip 
+		. " \"" . $name . "\"" 
+		. " \"" . $email . "\"" 
+		. " \"" . $owner . "\"" 
+		. " \"" . $primary . "\"" 
+		. " \"" . "Users" . "\""; */
+		exec("/home/marsPortal/freeradius-integration/self-registration/captive-portal-add_user_to_radius.sh " 
+		. $ip 
+		. " \"" . $name . "\"" 
+		. " \"" . $email . "\"" 
+		. " \"" . $owner . "\"" 
+		. " \"" . $primary . "\"" 
+		. " \"" . "Users" . "\"",
+		$output, $exitCode);
+	?>
 	</p>
-</span>
-<table>
-	<tr>
-		<td>
-			<img src="captiveportal-apzu.png" />
-		</td>
-		<td>
-			Everybody gets connected - Hopefully you by now as well!<br/>
-			<p><?php
-				$ip=$_SERVER['REMOTE_ADDR'];
-				$name=$_REQUEST['name'];
-				$email="";
-				$owner="";
-				$primary="";
-			    echo "The registration process should be done for " . $name . " (" . $email . ") from " . $ip;
-				echo exec("/home/marsPortal/freeradius-integration/self-registration/captive-portal-add_user_to_radius.sh " . $ip . " \"" . $name . "\" \"" . $email . "\" " . "\"" . $owner . "\" " . "\"" . $primary . "\"" . "Users" . "\"");
-			?>
-			</p>
-			<p>If you are not able to access any webpages like <a href="http://www.google.mw">Google</a>, please try it <a href="/">again</a> in a few minutes.</p>
-		</td>
-	</tr>
-</table>
+	<p>If you are not able to access any webpages like <a href="http://www.marsgeneral.com">Mars General</a>, please try it <a href="/">again</a> in a few minutes.</p>
+
+</div>
 </body>
 </html>
