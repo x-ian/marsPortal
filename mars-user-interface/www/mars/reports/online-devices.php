2eb8138bb522031632dc84fd96789f70cfb69b98
x-ian <x-ian@x-ian.net>
2018-03-25 09:29:17 +0200

    cleaned

--- mars-user-interface/www/mars/reports/online-devices.php
+++ mars-user-interface/www/mars/reports/online-devices.php
@@ -10,7 +10,7 @@ include '../menu.php';
 
 function online() {
 	return "
-		SELECT radacct.username AS username, userinfo.firstname AS firstname, userinfo.lastname AS lastname, userinfo.mac_vendor AS mac_vendor, userinfo.hostname AS hostname, userinfo.email AS email, userinfo.department AS department, userinfo.organisation AS organisation, userinfo.registration_date AS registration_date, radacct.FramedIPAddress as ipaddress, radacct.AcctStartTime as acctstart, radacct.AcctSessionTime, radacct.AcctInputOctets AS Upload, radacct.AcctOutputOctets AS Download 
+		SELECT radacct.username AS username, userinfo.firstname AS firstname, userinfo.lastname AS lastname, userinfo.mac_vendor AS mac_vendor, userinfo.hostname AS hostname, userinfo.email AS email, userinfo.department AS department, userinfo.organisation AS organisation, userinfo.registration_date AS registration_date, radacct.FramedIPAddress as ipaddress, radacct.AcctStartTime as acctstart, radacct.AcctSessionTime, radacct.AcctInputOctets AS Upload, radacct.AcctOutputOctets AS Download, DATE_ADD(radacct.acctstarttime, INTERVAL radacct.acctsessiontime second) as last_contact 
 		FROM radacct LEFT JOIN userinfo ON (radacct.Username = userinfo.Username) 
 		WHERE (radacct.AcctStopTime IS NULL OR radacct.AcctStopTime = '0000-00-00 00:00:00') AND DATE_ADD(radacct.acctstarttime, INTERVAL radacct.acctsessiontime second) > DATE_ADD(NOW(), INTERVAL -10 MINUTE) AND (radacct.Username LIKE '%') 
 		ORDER BY radacctid asc;";
@@ -21,12 +21,11 @@ function online() {
 	<tr>
 		<th>MAC address</th>
 		<th>Name</th>
-		<th>Email</th>
-		<th>MAC vendor</th>
 		<th>Hostname</th>
-		<th>Organisation</th>
+		<th>MAC vendor</th>
 		<th>IP address</th>
 		<th>Session Start</th>
+		<th>Last activity</th>
 	</tr>
 <?php
 $result = mysql_query(online()) or trigger_error(mysql_error()); 
@@ -35,12 +34,11 @@ while($row = mysql_fetch_array($result)){
 	echo "<tr>";
 	echo "<td><a href=/mars/userinfo/edit.php?username=" . $row['username'] . ">" . $row['username'] . "</a></td>";
 	echo "<td>" . $row['firstname'] . " " . $row['lastname'] . "</td>";
-	echo "<td>" . $row['email'] . "</td>";
-	echo "<td>" . $row['mac_vendor'] . "</td>";
 	echo "<td>" . $row['hostname'] . "</td>";
-	echo "<td>" . $row['organisation'] . "</td>";
+	echo "<td>" . $row['mac_vendor'] . "</td>";
 	echo "<td>" . $row['ipaddress'] . "</td>";
 	echo "<td>" . $row['acctstart'] . "</td>";
+	echo "<td>" . $row['last_contact'] . "</td>";
 	echo "</tr>";
 }
 ?>
