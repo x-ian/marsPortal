ef78ff1c68eab55d3b48c37d020bc81b9b36a722
x-ian <x-ian@x-ian.net>
2018-03-25 21:23:00 +0200

    asf

--- mars-user-interface/www/mars/admin/licenses.php
+++ mars-user-interface/www/mars/admin/licenses.php
@@ -11,14 +11,14 @@ marsPortal gratefully is built on top of the following open source project:
 	<thead><tr>
 		<th>Component</th><th>Description</th>
 	</tr></thead>
-	<tbody><tr>
-		<td><a href="https://www.pfsense.org/about-pfsense/">pfSense</a></td><td>This product includes software developed by the pfSense Project for use in the pfSense® software distribution. (http://www.pfsense.org/).</td>
-		<td><a href="https://www.freebsd.org/copyright/freebsd-license.html">FreeSB</a></td><td></td>
-		<td><a href="http://freeradius.org/">freeRADIUS</a></td><td>GPL v2</td>
-		<td><a href="http://www.mysql.com/">MySQL</a></td><td>GPL</td>
-		<td><a href="http://www.squid-cache.org/">Squid</a> &amp; <a href="http://lightsquid.sourceforge.net/Index2.html">Lightsquid</td><td>GPL v2</td>
-		<td><a href="http://tools.netsa.cert.org/yaf/libyaf/">yaf</a></td><td>GPL</td>
-		<td>bash/wget/curl/perl/php/ssmtp</td><td></td>
+	<tbody>
+		<tr><td><a href="https://www.pfsense.org/about-pfsense/">pfSense</a></td><td>This product includes software developed by the pfSense Project for use in the pfSense® software distribution. (http://www.pfsense.org/).</td></tr>
+		<tr><td><a href="https://www.freebsd.org/copyright/freebsd-license.html">FreeSB</a></td><td></td></tr>
+		<tr><td><a href="http://freeradius.org/">freeRADIUS</a></td><td>GPL v2</td></tr>
+		<tr><td><a href="http://www.mysql.com/">MySQL</a></td><td>GPL</td></tr>
+		<tr><td><a href="http://www.squid-cache.org/">Squid</a> &amp; <a href="http://lightsquid.sourceforge.net/Index2.html">Lightsquid</td><td>GPL v2</td></tr>
+		<tr><td><a href="http://tools.netsa.cert.org/yaf/libyaf/">yaf</a></td><td>GPL</td></tr>
+		<tr><td>bash/wget/curl/perl/php/ssmtp</td><td></td></tr>
 	</tr></tbody>
 </table>
 </div>
