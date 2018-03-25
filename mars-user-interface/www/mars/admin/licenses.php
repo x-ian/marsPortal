9861c18e0327b216c5c12b76a1607eea03c4f705
x-ian <x-ian@x-ian.net>
2018-03-25 21:16:31 +0200

    sdd

---
+++ mars-user-interface/www/mars/admin/licenses.php
@@ -0,0 +1,31 @@
+<? 
+include '../menu.php'; 
+?>
+
+<!-- begin page-specific content ########################################### -->
+    <div id="main">
+
+marsPortal gratefully is built on top of the following open source project:
+
+<table class='table table-striped'>
+	<thead><tr>
+		<th>Component</th><th>Description</th>
+	</tr></thead>
+	<tbody><tr>
+		<td><a href="https://www.pfsense.org/about-pfsense/">pfSense</a></td><td>This product includes software developed by the pfSense Project for use in the pfSense® software distribution. (http://www.pfsense.org/).</td>
+	</tr></tbody>
+</table>
+
+: : ""
+FreeBSD: https://www.freebsd.org/copyright/freebsd-license.html
+freeRADIUS: http://freeradius.org/ GPLv2
+MySQL: http://www.mysql.com/ GPL
+Squid: http://www.squid-cache.org/ / Lightsquid: http://lightsquid.sourceforge.net/Index2.html: GPLv2
+nTop, http://www.ntop.org/, GPLv3
+yaf: http://tools.netsa.cert.org/yaf/libyaf/: GPL
+bash/wget/curl/perl/php/ssmtp
+
+in software/web page/docs
+
+</div>
+</body>
