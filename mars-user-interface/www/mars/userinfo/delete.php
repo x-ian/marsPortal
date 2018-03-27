bba7e43cebe80e018902807991360f0225cda2aa
x-ian <x-ian@x-ian.net>
2018-03-27 06:08:44 +0200

    use php instead of external post to terminate session

--- mars-user-interface/www/mars/userinfo/delete.php
+++ mars-user-interface/www/mars/userinfo/delete.php
@@ -17,7 +17,7 @@ mysql_query("DELETE FROM `daily_accounting_v2` WHERE `username` = '$username' ")
 mysql_query("DELETE FROM `accounting_snapshot` WHERE `username` = '$username' ") ; 
 mysql_query("DELETE FROM `userinfo` WHERE `username` = '$username' ") ; 
 echo (mysql_affected_rows()) ? "Device including accounting history deleted.<br /> " : "Nothing deleted.<br /> "; 
-exec("/home/marsPortal/misc/captiveportal-disconnect-user.sh " . $username, $out, $exit);
+exec("/usr/local/bin/php -q /home/marsPortal/misc/captiveportal-disconnect-user.php  " . $username, $out, $exit);
 ?> 
 <b>Note: Deleting a device will automatically close its session on the Captive Portal.</b>
 
