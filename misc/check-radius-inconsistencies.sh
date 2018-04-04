a9de452fb9b76fd5bf10796d442b800e22318a23
x-ian <x-ian@x-ian.net>
2018-04-04 16:04:27 +0200

    checked

--- misc/check-radius-inconsistencies.sh
+++ misc/check-radius-inconsistencies.sh
@@ -7,8 +7,11 @@ source $BASEDIR/config.txt
 ENTRIES=`/usr/local/bin/mysql -u $MYSQL_USER -p$MYSQL_PASSWD radius -e "select username, count(*) from radusergroup group by username having count(*) <> 1;"`
 RET=`echo "$ENTRIES" | /usr/bin/wc -l`
 
+ERRORS=0
+
 if [ "$ENTRIES" != ""  ]; then
-  echo "Device(s) with more than one assigned groups detected"
+	ERRORS=1
+  echo "Device(s) with more than one assigned groups detected."
   echo $ENTRIES
 fi
 
@@ -16,14 +19,22 @@ ENTRIES=`/usr/local/bin/mysql -u $MYSQL_USER -p$MYSQL_PASSWD radius -e "select u
 RET=`echo "$ENTRIES" | /usr/bin/wc -l`
 
 if [ "$ENTRIES" != ""  ]; then
+	ERRORS=1
   echo "Device(s) without any group assignment detected."
   echo $ENTRIES
+else
+  echo "No inconsistency detected"
 fi
 
 ENTRIES=`/usr/local/bin/mysql -u $MYSQL_USER -p$MYSQL_PASSWD radius -e "select username, count(*) from userinfo group by username having count(*) <> 1;"`
 RET=`echo "$ENTRIES" | /usr/bin/wc -l`
 
 if [ "$ENTRIES" != ""  ]; then
-  echo "Device(s) with more than one userinfo entry detected"
+	ERRORS=1
+  echo "Device(s) with more than one userinfo entry detected."
   echo $ENTRIES
+fi
+
+if [ $ERRORS == 0 ]; then
+	echo "No inconsistencies detected."
 fi
