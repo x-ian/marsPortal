d8ed100ec3efaa4aa380259c0160cac4116c323c
x-ian <x-ian@x-ian.net>
2018-04-04 08:27:41 +0200

    extended

--- mars-user-interface/www/mars/wan/log_internet_ping.php
+++ mars-user-interface/www/mars/wan/log_internet_ping.php
@@ -15,11 +15,15 @@ $min_ago_5 = date('Y-m-d H:i:s', strtotime('-5 minutes'));
 $min_ago_15 = date('Y-m-d H:i:s', strtotime('-15 minutes'));
 $hour_ago_1 = date('Y-m-d H:i:s', strtotime('-1 hour'));
 $hour_ago_4 = date('Y-m-d H:i:s', strtotime('-4 hours'));
+$hour_ago_8 = date('Y-m-d H:i:s', strtotime('-8 hours'));
+$hour_ago_12 = date('Y-m-d H:i:s', strtotime('-12 hours'));
+$day_ago_1 = date('Y-m-d H:i:s', strtotime('-1 day'));
+$day_ago_7 = date('Y-m-d H:i:s', strtotime('-7 days'));
 ?>
 
 <table class='table table-striped'>
 	<thead><tr>
-		<td>Period</td><td>Internet Availability</td>
+		<td>Period</td><td>Internet Availability</td><td>Packets sent/received</td>
 	</tr></thead>
 	<tbody><tr>
 		<th>Last 5 minutes</th>
@@ -27,6 +31,7 @@ $hour_ago_4 = date('Y-m-d H:i:s', strtotime('-4 hours'));
 			<? $result = mysql_query(internet_ping(4, "interval 5 minute")) or trigger_error(mysql_error())?>
 			<?=get_percent(mysql_fetch_array($result)[3]) ?> %
 		</td>
+		<td><?=get_percent(mysql_fetch_array($result)[1]) ?> / <?=get_percent(mysql_fetch_array($result)[2]) ?></td>
 	</tr>
 	<tr>
 		<th>Last 15 minutes</th>
@@ -34,6 +39,7 @@ $hour_ago_4 = date('Y-m-d H:i:s', strtotime('-4 hours'));
 			<? $result = mysql_query(internet_ping(14, "interval 15 minute")) or trigger_error(mysql_error())?>
 			<?=get_percent(mysql_fetch_array($result)[3]) ?> %
 		</td>
+		<td><?=get_percent(mysql_fetch_array($result)[1]) ?> / <?=get_percent(mysql_fetch_array($result)[2]) ?></td>
 	</tr>
 	<tr>
 		<th>Last hour</th>
@@ -41,13 +47,47 @@ $hour_ago_4 = date('Y-m-d H:i:s', strtotime('-4 hours'));
 			<? $result = mysql_query(internet_ping(59, "interval 1 hour")) or trigger_error(mysql_error())?>
 			<?=get_percent(mysql_fetch_array($result)[3]) ?> %
 		</td>
+		<td><?=get_percent(mysql_fetch_array($result)[1]) ?> / <?=get_percent(mysql_fetch_array($result)[2]) ?></td>
 	</tr>
 	<tr>
 		<th>Last 4 hours</th>
 		<td>
-			<? $result = mysql_query(internet_ping(236, "interval 4 hour")) or trigger_error(mysql_error())?>
+			<? $result = mysql_query(internet_ping(239, "interval 4 hour")) or trigger_error(mysql_error())?>
 			<?=get_percent(mysql_fetch_array($result)[3]) ?> %
 		</td>
+		<td><?=get_percent(mysql_fetch_array($result)[1]) ?> / <?=get_percent(mysql_fetch_array($result)[2]) ?></td>
+	</tr>
+	<tr>
+		<th>Last 8 hours</th>
+		<td>
+			<? $result = mysql_query(internet_ping(479, "interval 8 hour")) or trigger_error(mysql_error())?>
+			<?=get_percent(mysql_fetch_array($result)[3]) ?> %
+		</td>
+		<td><?=get_percent(mysql_fetch_array($result)[1]) ?> / <?=get_percent(mysql_fetch_array($result)[2]) ?></td>
+	</tr>
+	<tr>
+		<th>Last 12 hours</th>
+		<td>
+			<? $result = mysql_query(internet_ping(719, "interval 12 hour")) or trigger_error(mysql_error())?>
+			<?=get_percent(mysql_fetch_array($result)[3]) ?> %
+		</td>
+		<td><?=get_percent(mysql_fetch_array($result)[1]) ?> / <?=get_percent(mysql_fetch_array($result)[2]) ?></td>
+	</tr>
+	<tr>
+		<th>Last day</th>
+		<td>
+			<? $result = mysql_query(internet_ping(1439, "interval 24 hour")) or trigger_error(mysql_error())?>
+			<?=get_percent(mysql_fetch_array($result)[3]) ?> %
+		</td>
+		<td><?=get_percent(mysql_fetch_array($result)[1]) ?> / <?=get_percent(mysql_fetch_array($result)[2]) ?></td>
+	</tr>
+	<tr>
+		<th>Last 7 days</th>
+		<td>
+			<? $result = mysql_query(internet_ping(10079, "interval 7 day")) or trigger_error(mysql_error())?>
+			<?=get_percent(mysql_fetch_array($result)[3]) ?> %
+		</td>
+		<td><?=get_percent(mysql_fetch_array($result)[1]) ?> / <?=get_percent(mysql_fetch_array($result)[2]) ?></td>
 	</tr></tbody>
 
 <?
