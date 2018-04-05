3936b3b664bc152c52d7e6a9be8ee63e853e9dcd
Christian Neumann <x-ian@x-ian.net>
2018-04-05 14:19:44 +0200

    wan interface configurable

--- misc/log_wan_traffic_2.sh
+++ misc/log_wan_traffic_2.sh
@@ -5,15 +5,15 @@ source $BASEDIR/config.txt
 
 LOG=/tmp/log_wan_traffic_2.log
 
-/usr/bin/netstat -i -b -n -I igb0 > $LOG
+/usr/bin/netstat -i -b -n -I $WAN_INTERFACE > $LOG
 #/usr/bin/netstat -i -b -n -I bridge0 >> $LOG
 
-TX=$(grep "igb0" $LOG | grep "Link" | awk '{print $11}')
+TX=$(grep "$WAN_INTERFACE" $LOG | grep "Link" | awk '{print $11}')
 if [ -z "$TX" ]; then
 	TX=-1
 fi
 TX_UNIT="bytes" #$(grep "today" $LOG | awk '{print $6}')
-RX=$(grep "igb0" $LOG | grep "Link" | awk '{print $8}')
+RX=$(grep "$WAN_INTERFACE" $LOG | grep "Link" | awk '{print $8}')
 if [ -z "$RX" ]; then
 	RX=-1
 fi
