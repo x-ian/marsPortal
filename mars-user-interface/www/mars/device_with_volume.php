d4d600ca6eb03df8af6d06464936d96a1f6ccc0d
Christian Neumann <x-ian@x-ian.net>
2018-04-15 16:11:50 +0200

    sdfsd

--- mars-user-interface/www/mars/device_with_volume.php
+++ mars-user-interface/www/mars/device_with_volume.php
@@ -1,9 +1,22 @@
+<? 
+$HEADLINE = 'Device history'; 
+include './menu.php'; 
+?>
+
+<?php $username = $_GET['username']; ?>
+
 <!-- begin page-specific content ########################################### -->
+    <div id="main">
+      <div class="page-header">
+  	    <h1>Traffic volume history for Device <?php echo $username . ' at ' . date('Y-m-d H:i:s'); ?></h1>
+	  </div>
+
+<!-- begin page-specific content ########################################### 
     <div id="main">
 
 <? 
 
-include 'config.php'; 
+//include 'config.php'; 
 
 ?>
 
@@ -14,17 +27,8 @@ include 'config.php';
 </span> 
 
 <hr/><br/>
-
-
-<?php $username = $_GET['username']; ?>
-
-<span style="font-variant:small-caps; font-size:200%">
-	<p align="center">
-		Data volume for device <?php echo $username . ' at ' . date('Y-m-d H:i:s')?>
-	</p>
-	<br/>
-</span>
-
+-->
+	  
 <?php
   function query($query) {
     $result = mysql_query($query);
@@ -41,47 +45,35 @@ include 'config.php';
   $daysago7 = date('Y-m-d', strtotime('-6 days'));
   $daysago30 = date('Y-m-d', strtotime('-29 days'));
   
-echo "<table>
-	<tr>
-		<th/>
-		<th>Working down</th>
-		<th>Working up</th>
-		<th>Non-Working down</th>
-		<th>Non-Working up</th>
-		<th>Total down</th>
+echo "<table class='table table-striped'>
+	<thead><tr>
+		<th>Day</th>
 		<th>Total up</th>
-	</tr>";
+		<th>Total down</th>
+	</tr></thead><tbody>";
 
   function user($username, $start) {
-	  return '
-select 
-	username, 
-	day, 
-	ROUND((day_total_input - day_offset_input) / 1000000) as total_input, 
-	ROUND((day_total_output - day_offset_output) / 1000000) as total_output, 
-	ROUND((work_total_input - work_offset_input - lunch_total_input + lunch_offset_input) / 1000000) as work_input,
-	ROUND((work_total_output - work_offset_output - lunch_total_output + lunch_offset_output) / 1000000) as work_output,
-	ROUND((day_total_input - day_offset_input - work_total_input + work_offset_input + lunch_total_input - lunch_offset_input) / 1000000) as non_work_input, 
-	ROUND((day_total_output - day_offset_output - work_total_output + work_offset_output + lunch_total_output - lunch_offset_output) / 1000000) as non_work_output
-from daily_accounting_v2 
-where username = "' . $username . '" and day > "' . $start . '" 
-group by username, day order by day desc;';  
-  }
+	  $a = '
+		  select 
+		  username, 
+		  day, 
+		  ROUND(SUM(0029_input+0059_input+0129_input+0159_input+0229_input+0259_input+0329_input+0359_input+0429_input+0459_input+0529_input+0559_input+0629_input+0659_input+0729_input+0759_input+0829_input+0859_input+0929_input+0959_input+1029_input+1059_input+1129_input+1159_input+1229_input+1259_input+1329_input+1359_input+1429_input+1459_input+1529_input+1559_input+1629_input+1659_input+1729_input+1759_input+1829_input+1859_input+1929_input+1959_input+2029_input+2059_input+2129_input+2159_input+2229_input+2259_input+2329_input+2359_input) / 1000000) as upload,
+		  ROUND(SUM(0029_output+0059_output+0129_output+0159_output+0229_output+0259_output+0329_output+0359_output+0429_output+0459_output+0529_output+0559_output+0629_output+0659_output+0729_output+0759_output+0829_output+0859_output+0929_output+0959_output+1029_output+1059_output+1129_output+1159_output+1229_output+1259_output+1329_output+1359_output+1429_output+1459_output+1529_output+1559_output+1629_output+1659_output+1729_output+1759_output+1829_output+1859_output+1929_output+1959_output+2029_output+2059_output+2129_output+2159_output+2229_output+2259_output+2329_output+2359_output) / 1000000) as download 
+		  FROM daily_accounting_v5
+		  where username = "' . $username . '" and day > "' . $start . '" 
+		  group by username, day order by day desc;';  
+		//echo $a;
+		return $a;
+	}
     
 $all_traffic = query(user($username, $daysago30));
 while ($row = mysql_fetch_assoc($all_traffic)) {
 	echo "<tr>";
 	echo '<td>' . $row['day'] . '</td>';
-    echo '<td>' . $row['work_output'] . '</td>';
-    echo '<td>' . $row['work_input'] . '</td>';
-    echo '<td>' . $row['non_work_output'] . '</td>';
-    echo '<td>' . $row['non_work_input'] . '</td>';
-    echo '<td>' . $row['total_output'] . '</td>';
-    echo '<td>' . $row['total_input'] . '</td>';
+    echo '<td>' . $row['upload'] . '</td>';
+    echo '<td>' . $row['download'] . '</td>';
 	echo "</tr>";
 }
 mysql_free_result($all_traffic);
 ?>
-</table>
-<br/>
-(Numbers updated every 10 minutes)
+</tbody></table>
