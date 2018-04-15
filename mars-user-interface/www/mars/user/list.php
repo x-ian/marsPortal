91510d96e0f63698828086adfd0e73ef2885ca73
x-ian <x-ian@x-ian.net>
2018-04-15 13:23:03 +0200

    new ui

--- mars-user-interface/www/mars/user/list.php
+++ mars-user-interface/www/mars/user/list.php
@@ -50,20 +50,21 @@ if (isset($_POST['submitted'])) {
 	}
 </script>
 	
-<? 
-echo "<table class='listtable'>"; 
-echo "<tr>"; 
-echo "<td><b>Name</b></td>"; 
-echo "<td><b>Group</b></td>"; 
-echo "<td><b>Department</b></td>"; 
-echo "<td><b>MAC address</b></td>"; 
-echo "<td><b>Join</b></td>"; 
-echo "<td><b>Mac Vendor</b></td>"; 
-echo "<td><b>Hostname</b></td>"; 
-echo "<td><b>Email</b></td>"; 
-echo "<td><b>Organisation</b></td>"; 
-echo "<td><b>Notes</b></td>"; 
-echo "</tr>"; 
+<table class='table table-striped'>
+<thead><tr>
+<td>Name</td>
+<td>Group</td>
+<td>Department</td>
+<td>MAC address</td>
+<td>Join</td>
+<td>Mac Vendor</td>
+<td>Hostname</td>
+<td>Email</td>
+<td>Organisation</td>
+<td>Notes</td>
+</tr></thead>
+<tbody>
+<?
 echo "<form action='' method='POST'>";
 
 $result = mysql_query("SELECT u.username AS username, u.firstname AS firstname, u.lastname AS lastname, u.mac_vendor AS mac_vendor, u.hostname AS hostname, u.email AS email, u.department AS department, u.organisation AS organisation, u.registration_date AS registration_date, u.initial_ip AS initial_ip, u.notes AS notes, radusergroup.groupname AS groupname FROM userinfo AS u LEFT JOIN radusergroup on u.username = radusergroup.username ORDER BY lastname, firstname ASC") or trigger_error(mysql_error()); 
@@ -82,7 +83,7 @@ if ($previous_name == $name) {
 	echo "<td>" . nl2br( $row['groupname']) . "</td>";  
 	echo "<td>" . nl2br( $row['department']) . "</td>";  
 }
-echo "<td><a href=/mars/userinfo/edit.php?username=" . nl2br($row['username']) . ">" . nl2br( $row['username']) . "</a></td>";  
+echo "<td>" . link_to_device($row) . "</td>";  
 echo "<td><input type='checkbox' name='mac[]'" . "' value='" . $row['username'] . "' onchange='ensureTwoCheckboxes();'></td> "; 
 echo "<td>" . nl2br( $row['mac_vendor']) . "</td>";  
 echo "<td>" . nl2br( $row['hostname']) . "</td>";  
@@ -92,7 +93,7 @@ echo "<td>" . nl2br( $row['notes']) . "</td>";
 echo "</tr>"; 
 } 
 echo "<tr><td><br/></td></tr><tr><td colspan='10'><input id='submitted' name='submitted' type='submit' value='Join device entries' disabled/> (unifies name, department & group; only active when two devices are selected)</td></tr>";
-echo "</form></table>"; 
+echo "</form></tbody></table>"; 
 ?>
 </div>
 </body>
