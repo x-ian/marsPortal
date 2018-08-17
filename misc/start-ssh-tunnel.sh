3a793f2c169e3d6cfff4def82c0e7003f1b86319
Christian Neumann <x-ian@x-ian.net>
2018-08-17 07:52:43 +0200

    longer time between killing and restarting ssh

--- misc/start-ssh-tunnel.sh
+++ misc/start-ssh-tunnel.sh
@@ -6,7 +6,7 @@ source $BASEDIR/config.txt
 
 echo "Stopping all tunnels"
 /usr/bin/killall ssh
-/bin/sleep 5
+/bin/sleep 120
 
 echo "Starting new tunnel, abort with CTRL-C"
 /usr/local/bin/autossh -M 0 -o "ExitOnForwardFailure=yes" -o "ConnectTimeout=10" -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -o "BatchMode=yes" -R $SSH_TUNNEL_PORT:localhost:22 -N ssh-tunnel@marsgeneral.com &
